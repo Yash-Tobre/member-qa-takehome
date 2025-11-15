d3a8290dae76bfb7878144ed15b2403c6681f718
Yash Vishwanath Tobre <60502700+Yash-Tobre@users.noreply.github.com>
2025-11-15 12:31:03 -0600

    Update main.py

--- main.py
+++ main.py
@@ -522,6 +522,6 @@ async def shutdown_event():
         except Exception as e:
             logging.error(f"Error during shutdown: {e}")
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT",8000)), reload=True)
+#if __name__ == "__main__":
+    #import uvicorn
+    #uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT",8000)), reload=True)
